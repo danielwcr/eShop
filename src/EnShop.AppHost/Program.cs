d7cddeace6d43612317e834888e4677810b33124
Daniel Weslley <daniel.weslley@dreamdev.io>
2024-10-07 17:13:38 +0100

    fix api project naming

--- src/EnShop.AppHost/Program.cs
+++ src/EnShop.AppHost/Program.cs
@@ -9,10 +9,10 @@ var postgres = builder.AddPostgres("postgres")
     .WithImage("ankane/pgvector")
     .WithImageTag("latest");
 
-var orderDb = postgres.AddDatabase("orderingdb");
+var orderingDb = postgres.AddDatabase("orderingdb");
 
 var orderingApi = builder.AddProject<Projects.Ordering_API>("ordering-api")
     .WithReference(rabbitMq)
-    .WithReference(orderDb);
+    .WithReference(orderingDb);
 
 builder.Build().Run();
